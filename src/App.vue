2ae4b766ff09967ce6a9682bb1bc876bfbcdcc75
elysableu <elysabward@gmail.com>
2025-12-23 00:04:52 -0700

    Seperate header into it's own components

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
+import Header from './components/layout/Header.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/Portfolio_logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <Header />
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <style scoped>
