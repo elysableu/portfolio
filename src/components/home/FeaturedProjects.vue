c708b440924715e7c4d0be9ae800628c8a0e2ddd
elysableu <elysabward@gmail.com>
2025-12-23 16:09:00 -0700

    Add Ticker Component to FeaturedProjects

--- src/components/home/FeaturedProjects.vue
+++ src/components/home/FeaturedProjects.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
+import Ticker from '@/components/ticker/Ticker.vue'
+import type { TickerItemType } from '@/types/data.models'
 
+interface Props {
+  featured: TickerItemType[]
+}
+
+defineProps<Props>()
 </script>
 
 <template>
-  
+  <div>
+    <Ticker :items="featured" />
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
