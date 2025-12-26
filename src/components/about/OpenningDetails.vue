dee057357628c875e0088d483b083f1d038584cc
elysableu <elysabward@gmail.com>
2025-12-26 13:59:15 -0700

    Create aboutView components and related child components

---
+++ src/components/about/OpenningDetails.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  <div class="openning">
+    <div class="headshot"></div>
+    <div class="personal-details"></div>
+    <div class="social-links"></div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
