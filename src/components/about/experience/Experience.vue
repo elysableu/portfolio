5fe5ba22bc0ab63c35eb36907aecc83b622b09a4
elysableu <elysabward@gmail.com>
2025-12-26 23:21:39 -0700

    Finish experience component and child compnent

--- src/components/about/experience/Experience.vue
+++ src/components/about/experience/Experience.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+  import ExperienceItem from './ExperienceItem.vue'
+  import type { ExperienceType } from '@/types/data.models'
 
+  interface Props {
+    experienceList: ExperienceType[]
+  }
+
+  defineProps<Props>()
 </script>
 
 <template>
-  <div></div>
+  <div class="experience">
+    <ExperienceItem
+      v-for="item in experienceList"
+      :key="item.id"
+      :experienceItem="item"
+    />
+  </div>
 </template>
 
 <style scoped>
--- src/components/about/experience/ExperienceItem.vue
+++ src/components/about/experience/ExperienceItem.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
+  import type { ExperienceType } from '@/types/data.models';
 
+  interface Props {
+    experienceItem: ExperienceType
+  }
+
+  defineProps<Props>()
 </script>
 
 <template>
-  <div></div>
+  <div class="experience-item">
+    <img alt="experienceItem.title" :src="experienceItem.logo" width="60" height="60" />
+    <h3>{{ experienceItem.title }}</h3>
+    <p>{{ experienceItem.company }}</p>
+    <p>{{ experienceItem.location }}</p>
+    <p>{{ experienceItem.locationType }}</p>
+    <p>{{ experienceItem.type }}</p>
+    <p>{{ experienceItem.startDate }}</p>
+    <p>{{ experienceItem.endDate }}</p>
+    <p>{{ experienceItem.current }}</p>
+    <p>{{ experienceItem.description }}</p>
+    <p v-for="(item, index) in experienceItem.responsibilities" :key="index">{{ item }}</p>
+    <p v-for="(item, index) in experienceItem.technologies" :key="index">{{ item }}</p>
+  </div>
 </template>
 
 <style scoped>
