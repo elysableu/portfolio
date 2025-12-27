13358ad70d6656519601bd35460c3634786623bf
elysableu <elysabward@gmail.com>
2025-12-26 21:34:13 -0700

    Create Journey and JourneyItem components

--- src/components/about/opening/journey/Journey.vue
+++ src/components/about/opening/journey/Journey.vue
@@ -1,9 +1,36 @@
 <script setup lang="ts">
+  import JourneyItem from './JourneyItem.vue'
+  import type { JourneyType, AboutImages } from '@/types/data.models'
 
+  interface Props {
+    journey: JourneyType,
+    images: AboutImages
+  }
+
+  const props = defineProps<Props>()
+
+    console.log('Journey - Images received:', props.images)
+console.log('Journey - teach path:', props.images.teach)
+console.log('Journey - computer path:', props.images.computer)
+
+  const getImage = (index: number) => {
+     const imagePath = index % 2 === 0 ? props.images.teach : props.images.computer
+  console.log(`Journey - Image for index ${index}:`, imagePath)
+  return imagePath
+    // return index % 2 === 0 ? props.images.teach : props.images.computer
+  }
 </script>
 
 <template>
-  <div></div>
+  <div class="journey">
+    <JourneyItem
+      v-for="(paragraph, index) in journey.paragraphs"
+      :key="index"
+      :image="getImage(index)"
+      :paragraph="paragraph"
+    />
+
+  </div>
 </template>
 
 <style scoped>
--- src/components/about/opening/journey/JourneyItem.vue
+++ src/components/about/opening/journey/JourneyItem.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+  interface Props {
+    image: string
+    paragraph: string
+  }
 
+  const props = defineProps<Props>()
+
+  console.log('JourneyItem - Image path:', props.image)
+console.log('JourneyItem - Full URL:', window.location.origin + props.image)
 </script>
 
 <template>
-  <div></div>
+  <div class="journey-item">
+    <img
+      alt="journey-icon"
+      class="journey-icon"
+      :src="image"
+      width="75"
+      height="75"
+    />
+    <p>{{ paragraph }}</p>
+  </div>
 </template>
 
 <style scoped>
