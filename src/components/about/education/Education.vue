8e9e045176706eb76faf88360ac74e31818810f7
elysableu <elysabward@gmail.com>
2025-12-26 23:12:16 -0700

    Finish education component and child component

--- src/components/about/education/Education.vue
+++ src/components/about/education/Education.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
+  import EducationItem from './EducationItem.vue'
+  import type { EducationType } from '@/types/data.models'
 
+  interface Props {
+    educationList: EducationType[]
+  }
+
+  defineProps<Props>()
 </script>
 
 <template>
-  <div></div>
+  <div class="education">
+    <EducationItem
+      v-for="item in educationList"
+      :key="item.id" 
+      :education-item="item"
+    />
+  </div>
 </template>
 
 <style scoped>
-  
+
 </style>
