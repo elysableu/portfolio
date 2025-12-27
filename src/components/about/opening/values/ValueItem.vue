b486b987cc9dfb7da7605fd75a349c9168ca3012
elysableu <elysabward@gmail.com>
2025-12-26 22:17:10 -0700

    Create Value and ValueItem components

--- src/components/about/opening/values/ValueItem.vue
+++ src/components/about/opening/values/ValueItem.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+  interface Props {
+    title: string,
+    description: string,
+    icon: string
+  }
 
+  defineProps<Props>()
 </script>
 
 <template>
-  <div></div>
+  <div class="value-item">
+    <div class="value-heading">
+      <h4>{{ title }}</h4>
+      <img
+      alt="value-icon"
+      class="value-icon"
+      :src="icon"
+      width="75"
+      height="75"
+    />
+    </div>
+    <p>{{ description }}</p>
+  </div>
 </template>
 
 <style scoped>
--- src/components/about/opening/values/Values.vue
+++ src/components/about/opening/values/Values.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
+  import ValueItem from './ValueItem.vue'
+  import type { Value } from '@/types/data.models'
 
+  interface Props {
+    values: Value[]
+  }
+
+  defineProps<Props>()
 </script>
 
 <template>
-  <div></div>
+  <div class="values">
+    <ValueItem
+      v-for="(value, index) in values"
+      :key="index"
+      :title="value.title"
+      :description="value.description"
+      :icon="value.icon"
+    />
+  </div>
 </template>
 
 <style scoped>
