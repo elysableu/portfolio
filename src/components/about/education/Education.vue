bf0949bc01aecf69f01b3648a4e59f36444caa67
elysableu <elysabward@gmail.com>
2026-01-12 15:10:43 -0700

    Update styling for education components

--- src/components/about/education/Education.vue
+++ src/components/about/education/Education.vue
@@ -10,15 +10,34 @@
 </script>
 
 <template>
-  <div class="education">
-    <EducationItem
-      v-for="item in educationList"
-      :key="item.id" 
-      :education-item="item"
-    />
+  <div class="education glass-card-dark">
+    <h2>Education</h2>
+    <div class="education-list">
+      <EducationItem
+        v-for="item in educationList"
+        :key="item.id"
+        :education-item="item"
+      />
+    </div>
   </div>
 </template>
 
 <style scoped>
+  .education {
+    display: flex;
+    flex-direction: column;
+    padding: var(--spacing-lg);
+  }
 
+  .education-list {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  .education h2 {
+    text-align: right;
+    font-size: 30px;
+    padding-bottom: 5px;
+  }
 </style>
