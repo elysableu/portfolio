95781118c9959c0839f698280142ac640c6b0426
elysableu <elysabward@gmail.com>
2026-01-18 16:45:26 -0700

    Add projectsLibrary to projectsView

--- src/views/projects/ProjectsView.vue
+++ src/views/projects/ProjectsView.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
   import { ref, onMounted } from 'vue'
   import { useData } from '@/composables/useData'
+
   import type { Project, ProjectBriefType, TickerItemType } from '@/types/data.models'
+
+  import ProjectsLibrary from '@/components/projects/projectsLibrary/ProjectsLibrary.vue'
 
   const { loading, error, getProjectsPageData } = useData()
   const projectsData = ref<{
     projects: Project[]
     featured: TickerItemType[]
-    current: ProjectBriefType
+    current: ProjectBriefType | undefined
     tags: string[]
     technologies: string[]
   } | null>(null)
@@ -24,8 +27,14 @@
     <div v-else-if="projectsData">
       <div class="projects">
         <h1>Projects</h1>
-        <div class="projects-content">
-          <ProjectBrief />
+        <div class="projects-library">
+         <ProjectsLibrary
+          :projects="projectsData.projects"
+          :featured="projectsData.featured"
+          :current="projectsData.current"
+          :tags="projectsData.tags"
+          :technologies="projectsData.technologies"
+         />
         </div>
       </div>
     </div>
@@ -33,11 +42,28 @@
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
+
+  .projects-container {
+    padding: var(--spacing-lg);
+  }
+
+  .projects {
+
+  }
+
+  .projects h1 {
+    text-align: right;
+  }
+
+  .projects-library {
+    padding-top: 10px;
+  }
+
+/* @media (min-width: 1024px) {
+  .projects {
     min-height: 100vh;
     display: flex;
     align-items: center;
   }
-}
+} */
 </style>
