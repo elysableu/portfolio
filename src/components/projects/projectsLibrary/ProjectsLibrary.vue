6517f6d78d06de4048e9a39638be9eba4b67e3ad
elysableu <elysabward@gmail.com>
2026-01-18 16:45:52 -0700

    Add projectBrief to projectslibrary and completed base components

---
+++ src/components/projects/projectsLibrary/ProjectsLibrary.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+  import { serializeProjectBrief } from '@/utils/serializer'
+
+  import type { Project, ProjectBriefType, TickerItemType } from '@/types/data.models'
+  import ProjectBrief from './ProjectBrief.vue'
+
+  interface Props {
+    projects: Project[]
+    featured: TickerItemType[]
+    current: ProjectBriefType | undefined
+    tags: string[]
+    technologies: string[]
+  }
+
+  defineProps<Props>()
+</script>
+
+<template>
+  <div class="library-container">
+    <div class="library">
+      <ProjectBrief
+        v-for="item in projects"
+        :key="item.id"
+        :project="serializeProjectBrief(item)"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .library-container {
+    padding: 20px;
+  }
+
+  .library {
+    display: flex;
+    gap: 10px;
+  }
+</style>
