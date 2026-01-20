2fe59c7ef7a896a88b55ae096ec1df235811635e
elysableu <elysabward@gmail.com>
2026-01-20 13:30:16 -0700

    Add ProjectDetail component to ProjectDetailView

--- src/views/projects/ProjectDetailView.vue
+++ src/views/projects/ProjectDetailView.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
+  import { useProject } from '@/composables/useProject'
+  import ProjectDetail from '@/components/projects/projectDetail/ProjectDetail.vue'
+
+  const { project, loading, error, notFound } = useProject()
 
 </script>
 
 <template>
-  <div>
-
+  <div class="project-detail-container">
+    <div v-if="loading" class="loading">Loading...</div>
+    <div v-else-if="error" class="error">{{ error }}</div>
+    <div v-else-if="notFound" class="not-found">404: Project Not Found</div>
+    <div v-else-if="project" class="project">
+      <ProjectDetail
+        :project="project"
+      />
+    </div>
   </div>
 </template>
 
